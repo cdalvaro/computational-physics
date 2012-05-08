642582f52f22391ae9e4ac317a169bf853bee37b
Carlos David <Carlos@alumnos140.cmlv.uam.es>
2012-05-08 10:30:54 +0200

    Initial Commit

---
+++ Vibration Normal Modes 2D/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Vibration Normal Modes 2D
+//
+//  Created by Carlos David on 5/8/12.
+//  Copyright (c) 2012 NEPTUNO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
