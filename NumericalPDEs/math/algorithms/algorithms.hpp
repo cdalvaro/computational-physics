44c5a7a190ecfed43014afe0585a9c81a6ecf1f1
Carlos <carlos.alvaro@citelan.es>
2018-08-17 00:02:06 +0200

    Add algorithms.hpp

---
+++ NumericalPDEs/math/algorithms/algorithms.hpp
@@ -0,0 +1,12 @@
+//
+//  algorithms.hpp
+//  NumericalPDEs
+//
+//  Created by Carlos David on 30/07/2018.
+//  Copyright © 2018 cdalvaro. All rights reserved.
+//
+
+#pragma once
+
+#include "eigenvalue/qr.hpp"
+#include "systems/linear.hpp"
